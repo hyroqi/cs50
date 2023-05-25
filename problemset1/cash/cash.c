10ff8783eecb0c0d6910d06a0f127b4588d573b1
Hyroqi <hiroq.i@outlook.com>
2023-05-25 09:57:30 +0800

    cash completion

---
+++ problemset1/cash/cash.c
@@ -0,0 +1,82 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int get_cents(void);
+int calculate_quarters(int cents);
+int calculate_dimes(int cents);
+int calculate_nickels(int cents);
+int calculate_pennies(int cents);
+
+int main(void)
+{
+    // Ask how many cents the customer is owed
+    int cents = get_cents();
+
+    // Calculate the number of quarters to give the customer
+    int quarters = calculate_quarters(cents);
+    cents = cents - quarters * 25;
+
+    // Calculate the number of dimes to give the customer
+    int dimes = calculate_dimes(cents);
+    cents = cents - dimes * 10;
+
+    // Calculate the number of nickels to give the customer
+    int nickels = calculate_nickels(cents);
+    cents = cents - nickels * 5;
+
+    // Calculate the number of pennies to give the customer
+    int pennies = calculate_pennies(cents);
+    cents = cents - pennies * 1;
+
+    // Sum coins
+    int coins = quarters + dimes + nickels + pennies;
+
+    // Print total number of coins to give the customer
+    printf("Change Owed: %i\n", coins);
+}
+
+int get_cents(void)
+{
+    // TODO
+    int cents = -1;
+    
+    while (cents < 0 || cents > 99)
+        cents = get_int("How many cents?: ");
+    return (cents);
+}
+
+int calculate_quarters(int cents)
+{
+    // TODO
+    int buff = 0;
+
+    buff = cents/25;
+    return (buff);
+}
+
+int calculate_dimes(int cents)
+{
+    // TODO
+    int buff = 0;    
+
+    buff = cents/10;    
+    return (buff);
+}
+
+int calculate_nickels(int cents)
+{
+    // TODO
+    int buff = 0;
+
+    buff = cents/5;
+    return (buff);
+}
+
+int calculate_pennies(int cents)
+{
+    // TODO
+    int buff = 0;   
+
+    buff = cents;     
+    return (buff);
+}
